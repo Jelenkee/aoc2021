7160290be5c05a5ccb0d3463b6c54474face3947
Jelenkee <ichhabadhs@posteo.de>
2021-12-18 15:47:21 +0100

    7.2

--- src/07/main.rs
+++ src/07/main.rs
@@ -6,7 +6,6 @@ mod util;
 fn main() {
     let mut vec: Vec<String> = util::read_file("07.txt");
     vec = vec.iter().filter(|s| !s.is_empty()).map(|s| String::from(s)).collect();
-    //vec[0]="16,1,2,0,4,2,7,1,2,14".to_string();
     let crabs = vec[0]
         .split(",")
         .map(|s| s.trim())
@@ -25,22 +24,48 @@ fn one(crabs: &Vec<u16>) {
     let mut least_fuel = u32::MAX;
     let mut least_fuel_pos = 0u16;
     for pos in min..=max {
-        let mut total_fuel=0u32;
+        let mut total_fuel = 0u32;
         for crab in crabs {
-            total_fuel+=(*crab as i32-pos as i32).abs() as u32;
+            total_fuel += (*crab as i32 - pos as i32).abs() as u32;
         }
-        println!("{}",total_fuel);
-        if(total_fuel<least_fuel){
-            least_fuel=total_fuel;
-            least_fuel_pos=pos;
+        if (total_fuel < least_fuel) {
+            least_fuel = total_fuel;
+            least_fuel_pos = pos;
         }
     }
-    let sum:i32=crabs.iter().map(|c|(*c as i32-least_fuel_pos as i32).abs()).sum();
-    println!("7.1: {}",sum);
+    let sum: i32 = crabs.iter().map(|c| (*c as i32 - least_fuel_pos as i32).abs()).sum();
+    println!("7.1: {}", sum);
 }
 
-fn two(crabs: &Vec<u16>) {}
+fn two(crabs: &Vec<u16>) {
+    let min: u16 = *crabs.iter().min().unwrap();
+    let max: u16 = *crabs.iter().max().unwrap();
 
+    let mut least_fuel = u32::MAX;
+    let mut least_fuel_pos = 0u16;
+    for pos in min..=max {
+        let mut total_fuel = 0u32;
+        for crab in crabs {
+            total_fuel += calc_fuel((*crab as i32 - pos as i32).abs()) as u32;
+        }
+        if (total_fuel < least_fuel) {
+            least_fuel = total_fuel;
+            least_fuel_pos = pos;
+        }
+    }
+    let sum: i32 = crabs.iter().map(|c| calc_fuel((*c as i32 - least_fuel_pos as i32).abs())).sum();
+    println!("7.2: {}", sum);
+}
+
+fn calc_fuel(steps: i32) -> i32 {
+    let mut cost_per_step = 1;
+    let mut total_cost = 0;
+    for i in 0..steps {
+        total_cost += cost_per_step;
+        cost_per_step += 1;
+    }
+    return total_cost;
+}
 
 
 
