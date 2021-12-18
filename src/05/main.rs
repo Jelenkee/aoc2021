828ad85f8211b82a45e1f5d4aae608a1fbce33fb
Jelenkee <ichhabadhs@posteo.de>
2021-12-18 11:48:01 +0100

    5.2

--- src/05/main.rs
+++ src/05/main.rs
@@ -15,8 +15,8 @@ fn main() {
 #[derive(Debug)]
 #[derive(PartialEq, Eq, Hash, Clone)]
 struct Point {
-    x: u16,
-    y: u16,
+    x: i32,
+    y: i32,
 }
 
 #[derive(Debug)]
@@ -26,11 +26,19 @@ struct Vector {
 }
 
 fn one(input: &Vec<String>) {
+    println!("5.1: {}", count_overlapping_points(input, false));
+}
+
+fn two(input: &Vec<String>) {
+    println!("5.2: {}", count_overlapping_points(input, true));
+}
+
+fn count_overlapping_points(input: &Vec<String>, diagonal: bool) -> i32 {
     let vectors = vectors(input);
-    let vectors2: Vec<&Vector> = vectors.iter().filter(|v| v.start.x == v.end.x || v.start.y == v.end.y).collect();
+    let vectors2: Vec<&Vector> = vectors.iter().collect();
     let mut points: Vec<Point> = vec![];
     for vector in vectors2 {
-        for p in all_point(vector) {
+        for p in all_point(vector, diagonal) {
             points.push(p);
         }
     }
@@ -45,12 +53,7 @@ fn one(input: &Vec<String>) {
             cc += 1;
         }
     }
-    println!("5.1: {}", cc);
-}
-
-fn two(input: &Vec<String>) {
-    let vectors = vectors(input);
-    let vectors2: Vec<&Vector> = vectors.iter().collect();
+    cc
 }
 
 fn vectors(input: &Vec<String>) -> Vec<Vector> {
@@ -60,30 +63,56 @@ fn vectors(input: &Vec<String>) -> Vec<Vector> {
         let coords2: Vec<&str> = points[1].split(",").collect();
         return Vector {
             start: Point {
-                x: u16::from_str(coords1[0]).unwrap(),
-                y: u16::from_str(coords1[1]).unwrap(),
+                x: i32::from_str(coords1[0]).unwrap(),
+                y: i32::from_str(coords1[1]).unwrap(),
             },
             end: Point {
-                x: u16::from_str(coords2[0]).unwrap(),
-                y: u16::from_str(coords2[1]).unwrap(),
+                x: i32::from_str(coords2[0]).unwrap(),
+                y: i32::from_str(coords2[1]).unwrap(),
             },
         };
     }).collect();
 }
 
-fn all_point(vector: &Vector) -> HashSet<Point> {
+fn all_point(vector: &Vector, diagonal: bool) -> HashSet<Point> {
     let mut set: HashSet<Point> = HashSet::new();
     if vector.start.x == vector.end.x {
-        let min = min(vector.start.y,vector.end.y);
-        let max = max(vector.start.y,vector.end.y);
+        let min = min(vector.start.y, vector.end.y);
+        let max = max(vector.start.y, vector.end.y);
         for y in min..=max {
             set.insert(Point { x: vector.start.x, y });
         }
-    } else {
-        let min = min(vector.start.x,vector.end.x);
-        let max = max(vector.start.x,vector.end.x);
+    } else if vector.start.y == vector.end.y {
+        let min = min(vector.start.x, vector.end.x);
+        let max = max(vector.start.x, vector.end.x);
         for x in min..=max {
             set.insert(Point { x, y: vector.start.y });
+        }
+    } else if diagonal && (vector.start.x - vector.end.x).abs() == (vector.start.y - vector.end.y).abs() {
+        let xUp = vector.start.x < vector.end.x;
+        let yUp = vector.start.y < vector.end.y;
+        let min_x = min(vector.start.x, vector.end.x);
+        let max_x = max(vector.start.x, vector.end.x);
+        let min_y = min(vector.start.y, vector.end.y);
+        let max_y = max(vector.start.y, vector.end.y);
+        let mut x = vector.start.x;
+        let mut y = vector.start.y;
+        loop {
+            set.insert(Point { x, y });
+            if xUp {
+                x += 1;
+            } else {
+                x -= 1;
+            }
+            if yUp {
+                y += 1;
+            } else {
+                y -= 1;
+            }
+            let pred = || if xUp { x > max_x } else { x < min_x };
+            if pred() {
+                break;
+            }
         }
     }
     return set;
