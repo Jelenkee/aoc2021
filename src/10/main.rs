8af6546e8b0ee556c4cc659c54f93227a5de0a1f
Jelenkee <ichhabadhs@posteo.de>
2021-12-27 18:00:35 +0100

    10.2

--- src/10/main.rs
+++ src/10/main.rs
@@ -16,31 +16,55 @@ fn one(input: &Vec<String>) {
     let closings = vec![')', ']', '}', '>'];
 
     let mut score = 0;
-
-    for s in input {
-        let mut stack = VecDeque::new();
-        for (i, c) in s.chars().enumerate() {
-            if openings.contains(&c) {
-                stack.push_front(c);
-            } else {
-                if !closings.contains(&c) {
-                    panic!("WTF??!");
-                }
-                let el = stack.pop_front().unwrap();
-                if index_of(&openings, &el) != index_of(&closings, &c) {
-                    score += match c {
-                        ')' => { 3 }
-                        ']' => { 57 }
-                        '}' => { 1197 }
-                        '>' => { 25137 }
-                        _ => { panic!("AARGH!!") }
-                    };
-                    break;
+    let incomplete_lines: Vec<(&String, VecDeque<char>)> = input.iter()
+        .map(|s| {
+            let mut stack = VecDeque::new();
+            for c in s.chars() {
+                if openings.contains(&c) {
+                    stack.push_front(c);
+                } else {
+                    if !closings.contains(&c) {
+                        panic!("WTF??!");
+                    }
+                    let el = stack.pop_front().unwrap();
+                    if index_of(&openings, &el) != index_of(&closings, &c) {
+                        score += match c {
+                            ')' => { 3 }
+                            ']' => { 57 }
+                            '}' => { 1197 }
+                            '>' => { 25137 }
+                            _ => { panic!("AARGH!!") }
+                        };
+                        return (s, false, stack);
+                    }
                 }
             }
-        }
-    }
+            return (s, true, stack);
+        })
+        .filter(|t| t.1)
+        .map(|t| (t.0, t.2))
+        .collect();
+
     println!("10.1: {}", score);
+    let mut scores: Vec<i64> = vec![];
+    for (l, stack) in incomplete_lines {
+        let mut subscore = 0i64;
+        stack.iter()
+            .map(|cc| closings[index_of(&openings, cc)])
+            .for_each(|cc| {
+                subscore *= 5;
+                subscore += match cc {
+                    ')' => { 1 }
+                    ']' => { 2 }
+                    '}' => { 3 }
+                    '>' => { 4 }
+                    _ => { panic!("AARGH!!") }
+                };
+            });
+        scores.push(subscore);
+    }
+    scores.sort();
+    println!("10.2: {}", scores[scores.len() / 2]);
 }
 
 fn index_of(v: &Vec<char>, c: &char) -> usize {
